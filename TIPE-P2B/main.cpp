d8f8d7d7ff73c36ed3cd0ee7592871cee2f18487
Marc Partensky <marc.partensky@gmail.com>
2019-11-21 17:13:30 +0100

    Initial Commit

---
+++ TIPE-P2B/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TIPE-P2B
+//
+//  Created by Marc Partensky on 21/11/2019.
+//  Copyright © 2019 Marc Partensky. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
