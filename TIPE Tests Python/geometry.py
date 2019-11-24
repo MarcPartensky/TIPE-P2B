2d93d82cf59ef16b84cf06cc7ab609973f3db693
Marc Partensky <marc.partensky@gmail.com>
2019-11-24 22:14:53 +0100

    test

--- TIPE Tests Python/geometry.py
+++ TIPE Tests Python/geometry.py
@@ -3,13 +3,13 @@ import mycolors
 
 
 class Circle:
-    def __init__(self,center,
-                    radius,
-                    size=1,
-                    display=True,
-                    material=False,
-                    color=mycolors.BLACK,
-                    window=None):
+    def __init__(self, center,
+                 radius,
+                 size=1,
+                 display=True,
+                 material=False,
+                 color=mycolors.BLACK
+                 ):
         """Create a circle using its arguments:
         - tuple center : point of the circle's center
         - int   radius : circle's radius
@@ -22,29 +22,23 @@ class Circle:
         self.display = display
         self.material = material
         self.color = color
-        if material:
-            self.material()
-        if display and window:
-            self.show(window)
 
-    def show(self,window):
+    def show(self, window):
         """Affiche le segment à l'écran,
         s'il ne doit pas l'être l'object sera invisible"""
-        self.display = True
-        pygame.draw.circle(window.screen,self.color,self.center,self.radius)
+        if self.display:
+            pygame.draw.circle(window.screen, self.color,
+                               self.center, self.radius)
 
-    def material(self):
-        """Donne matière au segment et donc doit impliquer
-        des collision avec d'autre object matériel"""
-        self.material = True
-        raise NotImplementedError
 
 class Segment:
-    def __init__(self,p1,
-                    p2,
-                    size=1,
-                    material=False,
-                    color=mycolors.RED):
+    def __init__(self, p1,
+                 p2,
+                 size=1,
+                 material=False,
+                 color=mycolors.RED,
+                 display=True
+                 ):
         """Create a Segment using its arguments:
         - tuples p1, p2 : points of the segment extremities
         - int    size : thickness of the segment (1px by default)
@@ -53,35 +47,27 @@ class Segment:
         self.p1 = p1
         self.p2 = p2
         self.size = size
-        self.display = False
-        self.material = False # sera changer lors de l'appel de la méthode material()
+        self.material = material # sera changer lors de l'appel de la méthode material()
         self.color = color
-        if material:
-            self.material()
+        self.display = display
 
-    def show(self,window):
+    def show(self, window):
         """Affiche le segment à l'écran,
         s'il ne doit pas l'être l'object sera invisible"""
-        pygame.draw.line(window.screen,self.color,self.p1,self.p2)
-
-    def material(self):
-        """Donne matière au segment et donc doit impliquer
-        des collision avec d'autre object matériel"""
-        self.material = True
-        raise NotImplementedError
+        if self.display:
+            pygame.draw.line(window.screen, self.color, self.p1,self.p2, 1)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     from mywindow import Window
-    from time import sleep # This is a test
-    w = Window(size=[600,400],call=True)
+    w = Window(size=[600, 400], call=True)
     w.clear()
-    c = Circle(center=[300,200],radius=50)
-    s = Segment(p1=[100,100],p2=[500,300])
-
+    c = Circle(center=[300, 200], radius=50)
+    s = Segment(p1=[100, 100], p2=[500, 300], color=mycolors.RED)
     w.clear(color=mycolors.WHITE)
-    while w.check():
 
+    while w.open:
+        w.check()
         c.show(w)
         s.show(w)
         w.flip()
